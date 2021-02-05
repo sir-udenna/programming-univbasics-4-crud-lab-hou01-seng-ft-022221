e21fcf8fcff964d258a279a7617556964b02050f
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-05 06:53:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   family = ["mom", "dad", "sisters", "brother"]
-  family.shift("wow")
+  family.pop
 end
 
 def remove_element_from_start_of_array(array)
