fa36a4a7b9263523b2a1c403b7b7adbd8327921d
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-05 06:55:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   family = ["mom", "dad", "sisters", "brother"]
-  family.unshift("wow")
+  family.shift("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
