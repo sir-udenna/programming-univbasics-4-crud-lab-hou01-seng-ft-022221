321a79ffc2e049b94939fc5be16c164df039ddd4
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-05 06:56:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  family = ["mom", "dad", "sisters", "brother"]
-  family.shift("wow")
+  family = ["mom", "dad", "sisters", "wow"]
+  family.shift
 end
 
 def retrieve_element_from_index(array, index_number)
